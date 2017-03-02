b4c666c6b302cd285096a8dec05f7717ea7faaf8
Deniz <burakdeniz@mail.weber.edu>
2017-03-02 03:16:38 +0000

    SRC Files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+      return (
+
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+         
+          <h2>Deniz App - Github Hello World</h2>
+          <p><h3><i> Assignment</i></h3></p> 
+        </div>
+
+        <p className="App-intro">
+        <h1>finally this is done</h1></p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
